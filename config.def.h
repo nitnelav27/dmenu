a3b671737bd3f8ad120f10c20ad3833bb13659c8
Valentin Vergara Hidd <valentinvergara@gmail.com>
2021-03-29 02:04:55 -0400

    changed colors

--- config.def.h
+++ config.def.h
@@ -11,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#222222", "#cf4b3e" },
+	[SchemeSel] = { "#222222", "#cf4b3e" },
+	[SchemeOut] = { "#000000", "#3ec2cf" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
